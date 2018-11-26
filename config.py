afb19356384f5517a57e0276b0a75e32cbe4cd50
Pavlo Maistruk <pavlomajstruk@gmail.com>
2018-11-26 14:00:50 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+TOKEN = os.environ['TOKEN']
+APP_NAME = 'bo-ts'
+DB_NAME = os.environ['DB_NAME']
+DB_HOST = os.environ['DB_HOST']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_USER = os.environ['DB_USER']
