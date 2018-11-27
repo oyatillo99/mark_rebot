0365565d95516ca1fce293b18604790791557003
Pavlo Maistruk <pavlomajstruk@gmail.com>
2018-11-27 09:03:24 +0200

    Update run_server.py

--- run_server.py
+++ run_server.py
@@ -23,5 +23,4 @@ def index():
  
  
 if __name__ == "__main__":
-    print('Staring server webhook no set')
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
