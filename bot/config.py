c985c67fb8509d62c9e7b6f9a2681409cf445733
oyatillo99 <52331212+oyatillo99@users.noreply.github.com>
2019-06-29 00:47:11 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,8 @@
 import os
 
 
-TOKEN = os.environ['TOKEN']
-APP_NAME = os.environ['APP_NAME']
+TOKEN = os.environ['832354568:AAF-0KaBTXkrQF7eY18Bk5_jOTxK8usfpDk']
+APP_NAME = os.environ['telebotuzb']
 DB_NAME = os.environ['DB_NAME']
 DB_HOST = os.environ['DB_HOST']
 DB_PASSWORD = os.environ['DB_PASSWORD']
