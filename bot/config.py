daaad5888c0d479f5970f4c2e07a506c9cf1b248
oyatillo99 <52331212+oyatillo99@users.noreply.github.com>
2019-06-30 02:45:03 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,7 @@
 import os
 
 
-TOKEN = os.environ['TOKEN']
+TOKEN = os.environ['832354568:AAF-0KaBTXkrQF7eY18Bk5_jOTxK8usfpDk']
 APP_NAME = os.environ['APP_NAME']
 DB_NAME = os.environ['DB_NAME']
 DB_HOST = os.environ['DB_HOST']
